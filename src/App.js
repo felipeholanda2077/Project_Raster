0b62afa3760d7fbe0c7e185f8ad588f1db2cc904
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-02-24 20:28:46 -0300

    Melhoramento de API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import "./App.css";
 import TrackingResult from "./components/TrackingResult";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Logo from "./img/Raster.png";
+import Swal from 'sweetalert2'
 
 function App() {
   const [data, setData] = useState([]);
@@ -16,26 +21,56 @@ function App() {
       .then(result => {
         if (result.message === "OK") {
           setData(result.data);
+
+          Swal.fire(
+            'Pedido Verificado com Sucesso!',
+            'Abaixo você pode ver o status do seu pedido!',
+            'success'
+          )
         }
-      })
-      .catch(console.error);
+      },
+    (err) => {
+      console.log('ERRO: ', err);
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro ao Buscar o pedido!',
+        text: 'Tente novamente mais tarde!',
+        footer: '<a href="">Por que eu tenho esse problema?</a>'
+      }
+      )
+    });
   };
 
-  return (
-    <div className="container">
-      <h1>React Tracking</h1>
-      <form onSubmit={submitHanlder}>
-        <div className="form-group">
-          <input type="text" className="form-control" name="tracking" />
+    return (
+      <div className="App">
+        <div className="container">
+          <img src={Logo} alt="Logo" />
+          <h1>Project Raster</h1>
+          <p>Verifique sua encomenda a seguir</p>
+          <form onSubmit={submitHanlder}>
+            <div className="form-group">
+              <input type="text" placeholder="AA123456789BR ou 000.111.222-33" className="form-control" name="tracking" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Verificar
+            </button>
+          </form>
+
+          <TrackingResult data={data} />
         </div>
-        <button type="submit" className="btn btn-primary">
-          Verificar
-        </button>
-      </form>
+        <footer>
+          <p>
+            Feito por <span role="img"></span>Felipe Holanda{" - "}
+            <a href="#">
+              <strong>Project Raster</strong>
+            </a>
+          </p>
+        </footer>
+      </div>
+    );
 
-      <TrackingResult data={data} />
-    </div>
-  );
-}
+  }
 
-export default App;
+
+  export default App;
