22048e2074df40c6220074bf5dd0c1481b59dfb9
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-02-27 23:57:47 -0300

    Adição de novas páginas

---
+++ src/Pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1>About</h1>
+            <p className="about">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                ultricies, nisl nec tincidunt luctus, nunc nisl aliquet mauris, ut
+                aliquet nunc nisl eget nunc. Sed euismod, nisl nec tincidunt
+                luctus, nunc nisl aliquet mauris, ut aliquet nunc nisl eget nunc.
+                Sed euismod, nisl nec tincidunt luctus, nunc nisl aliquet mauris, ut
+                aliquet nunc nisl eget nunc. Sed euismod, nisl nec tincidunt
+                luctus, nunc nisl aliquet mauris, ut aliquet nunc nisl eget nunc.
+                Sed euismod, nisl nec tincidunt luctus, nunc nisl aliquet mauris, ut
+                aliquet nunc nisl eget nunc. Sed euismod, nisl nec tincidunt
+                luctus, nunc nisl aliquet mauris, ut aliquet nunc nisl eget nunc.
+            </p>
+
+
+            <div>
+                <h1>About</h1>
+            
+            </div>
+        </div>
+
+    );
+}
+
+export default About;
---
+++ src/Pages/Maps.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Maps = () => {
+    return (
+        <div>
+        <h1>About</h1>
+        <p className="about">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            ultricies, nisl nec tincidunt luctus, nunc nisl aliquet mauris, ut
+            aliquet nunc nisl eget nunc. Sed euismod, nisl nec tincidunt
+            luctus, nunc nisl aliquet mauris, ut aliquet nunc nisl eget nunc.
+            Sed euismod, nisl nec tincidunt luctus, nunc nisl aliquet mauris, ut
+            aliquet nunc nisl eget nunc. Sed euismod, nisl nec tincidunt
+            luctus, nunc nisl aliquet mauris, ut aliquet nunc nisl eget nunc.
+            Sed euismod, nisl nec tincidunt luctus, nunc nisl aliquet mauris, ut
+            aliquet nunc nisl eget nunc. Sed euismod, nisl nec tincidunt
+            luctus, nunc nisl aliquet mauris, ut aliquet nunc nisl eget nunc.
+            </p>
+        <div>
+            <h1>About</h1>
+            
+        </div>
+        </div>
+
+    );
+}
+
+export default Maps;
