c5ef42a6199200d439aa032d90367e29cd7d2edb
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-02-27 21:29:55 -0300

    Modificações no sistema de rastreamento e sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+/* eslint-disable react/style-prop-object */
 /* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
+
+import w3_open from "./components/Sidebar";
+import w3_close from "./components/Sidebar";
+
 import "./App.css";
 import TrackingResult from "./components/TrackingResult";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+import Raster from "./img/RASTER1.png"
 import Logo from "./img/Raster.png";
 import Bag from "./img/Bag.png";
 import Chat from "./img/Chat.png";
@@ -14,12 +19,17 @@ import Play from "./img/Play.png";
 
 import Brasil from "./countries/brasil.png";
 
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+
 
 
 function App() {
   const [data, setData] = useState([]);
   const [valor, setValor] = React.useState('');
+
+
+
+
 
   const submitHanlder = event => {
     event.preventDefault();
@@ -66,18 +76,44 @@ function App() {
 
   return (
 
+
+
+
     <div className="backgroundrect position-absolute">
+
+      <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" className="d-none" id="mySidebar">
+        <img src={Raster} alt="Logo" className="img-fluid" />
+        <button type="button" onClick={w3_close} class="btn btn-lg float-right" id="closeNav"><i class="fa-solid fa-xmark" ></i></button>
+        <a href="#" class=" w3-bar-item btn  btn-lg btn-block my-5"><i class="fa-solid fa-house fa-lg"></i><strong> Home </strong><i class="fa-solid fa-arrow-right float-right mt-1"></i></a>
+        <a href="#" class=" w3-bar-item btn  btn-lg btn-block  my-5 "><i class="fa-solid fa-map fa-lg "></i><strong> Mapa Mundi </strong><i class="fa-solid fa-arrow-right float-right mt-1"></i></a>
+        <a href="#" class=" w3-bar-item btn  btn-lg btn-block my-5"><i class="fa-solid fa-user-tie fa-lg "></i><strong> Sobre Nós </strong><i class="fa-solid fa-arrow-right float-right mt-1"></i></a>
+        <a href="#" class=" w3-bar-item btn  btn-lg btn-block my-5"><i class="fa-solid fa-right-to-bracket fa-lg"></i><strong> Login </strong><i class="fa-solid fa-arrow-right float-right mt-1"></i></a>
+          
+
+
+
+        
+
+
+      </div>
+
+
+
+
+
       <header>
 
         <div className="row-sm">
           <div className="col-sm">
 
-            <button type="button" class="btn btn-outline-light mt-5 float-left"><i class="fa-solid fa-bars" /></button>
-
+            <button type="button" class="btn btn-outline-light mt-5" onClick={w3_open}><i class="fa-solid fa-bars" /></button>
           </div>
 
 
+
+
           <div className="col-sm">
+
           </div>
 
 
@@ -100,19 +136,13 @@ function App() {
           </div>
         </div>
 
-
-
-
-
-
-
         <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path class="wave1" fill="#43818A" fill-opacity="1" d="M0,96L30,117.3C60,139,120,181,180,213.3C240,245,300,267,360,240C420,213,480,139,540,122.7C600,107,660,149,720,170.7C780,192,840,192,900,176C960,160,1020,128,1080,112C1140,96,1200,96,1260,128C1320,160,1380,224,1410,256L1440,288L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
           <path class="wave2" fill="#57A7B3" fill-opacity="1" d="M0,288L30,266.7C60,245,120,203,180,181.3C240,160,300,160,360,165.3C420,171,480,181,540,170.7C600,160,660,128,720,112C780,96,840,96,900,96C960,96,1020,96,1080,128C1140,160,1200,224,1260,250.7C1320,277,1380,267,1410,261.3L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
           <path class="wave3" fill="#66C3D0" fill-opacity="1" d="M0,288L30,261.3C60,235,120,181,180,186.7C240,192,300,256,360,272C420,288,480,256,540,240C600,224,660,224,720,186.7C780,149,840,75,900,85.3C960,96,1020,192,1080,234.7C1140,277,1200,267,1260,261.3C1320,256,1380,256,1410,256L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
           <path fill="#66C3D2" fill-opacity="1" d="M0,192L60,202.7C120,213,240,235,360,256C480,277,600,299,720,309.3C840,320,960,320,1080,272C1200,224,1320,128,1380,80L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
 
         </svg>
-      </header>
+      </header >
 
       <div className="App mt-5 pt-5">
 
@@ -149,10 +179,6 @@ function App() {
         </div>
       </div>
 
-
-
-
-
       <main className="cards" class="cards">
 
         <section class="card contact ">
@@ -181,12 +207,6 @@ function App() {
         </section>
 
 
-
-
-
-
-
-
       </main>
 
       <footer id="rodape" className="d-flex justify-content-center bg-dark">
@@ -197,9 +217,7 @@ function App() {
           </a>
         </p>
       </footer>
-    </div>
-
-
+    </div >
 
 
 
@@ -209,6 +227,8 @@ function App() {
   );
 
 }
+
+
 
 <script src="https://kit.fontawesome.com/63c64f64f0.js" crossorigin="anonymous"></script>
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+
+export function w3_open() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+  }
+
+ export default function w3_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
+  
+  
