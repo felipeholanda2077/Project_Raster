7e2893675bbefa87bf2b921fff20e310905679b7
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-02-27 23:57:31 -0300

    Organização de Páginas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.scss';
-import App from './App';
+import '../src/Pages/main.scss';
+import Home from '../src/Pages/Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//import ReactDOM from "react-dom/client";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Home from "./Pages/Home";
+//import About from "./Pages/About";
+//import Maps from "./Pages/Maps";
+//import Layout from "./Pages/Layout";
+//import NoPage from "./Pages/NoPage";
+
+
+//export default function App() {
+//    return (
+//        <BrowserRouter>
+//            <Routes>
+//                <Route path="/" element={<Layout />}>
+//                    <Route index element={<Home />} />
+//                    <Route path="about" element={<About />} />
+//                    <Route path="maps" element={<Maps />} />
+//                    <Route path="*" element={<NoPage />} />
+//                </Route>
+//            </Routes>
+//        </BrowserRouter>
+//    );
+//}//
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(<App />);
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+
+import About from "./Pages/About";
+import Maps from "./Pages/Maps";
+import Layout from "./Pages/Layout";
+
+
+const Routes = () => {
+   return(
+       <BrowserRouter>
+           <Route component = { Layout }  path="/" exact />
+           <Route component = { Maps }  path="/maps" />
+           <Route component = { About }  path="/about" />
+           
+       </BrowserRouter>
+   )
+}
+
+export default Routes;
